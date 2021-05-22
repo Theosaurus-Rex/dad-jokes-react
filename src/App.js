90f427d7ebc5129e00a73519397c0d88428d65bd
Theosaurus-Rex <theosaurus13@gmail.com>
2021-05-22 12:36:52 +1000

    save search input value in app state

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ class App extends React.Component {
     super()
     // Initialise component state
     this.state = {
+      searchTerm: '',
       jokes: [],
       isFetchingJoke: false
       
     }
     this.onTellJoke = this.onTellJoke.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
   }
 
   // Load a joke to initially appear on page load
@@ -47,11 +49,20 @@ class App extends React.Component {
     this.searchJokes()
   }
 
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="Search for a joke..."/>
+          <input 
+            type="text" 
+            placeholder="Search for a joke..." 
+            onChange={this.onSearchChange}
+          />
           <button>Search</button>
           <button onClick={this.onTellJoke} disabled={this.state.isFetchingJoke}>Tell me a joke!</button>
         </form>
