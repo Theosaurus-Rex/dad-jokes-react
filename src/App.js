7d9fdeca6e04ce813bdb49df4c6a78c8022d28f3
Theosaurus-Rex <theosaurus13@gmail.com>
2021-05-22 11:55:17 +1000

    make joke render on screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
+import React from 'react';
 import './App.css';
 
-function App(props) {
+class App extends React.Component {
+  
+
+  constructor() {
+    super()
+    // Initialise component state
+    this.state = {
+      joke: null
+    }
+    this.onTellJoke = this.onTellJoke.bind(this)
+  }
   // Fetch random dad joke from API
-  const onTellJoke = () => {
+  onTellJoke() {
     fetch("https://icanhazdadjoke.com/", {
       method: 'GET',
       headers: {
@@ -11,11 +22,26 @@ function App(props) {
       }
     })
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => {
+        // Update state to joke string
+        this.setState({ joke: json.joke })
+      })
   }
-  return (
-    <button onClick={onTellJoke}>Tell me a joke!</button>
-  );
+
+  render() {
+    console.log('----- RENDER -----')
+
+    return (
+      <div>
+        <button onClick={this.onTellJoke}>Tell me a joke!</button>
+        <p>{this.state.joke}</p>
+      </div>
+      
+    );
+  }
+  
+
+  
 }
 
 export default App;
